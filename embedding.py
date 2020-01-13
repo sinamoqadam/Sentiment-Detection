5f1a85421eaab0b43faf2dd6853a73d13823c351
Sina Moqadam <sina.moqadam@gmail.com>
2020-01-13 15:04:58 +0330

    create embedding layer from glove data

---
+++ embedding.py
@@ -0,0 +1,41 @@
+import os
+import numpy as np
+from keras.layers import Embedding
+from keras.initializers import Constant
+
+BASE_DIR = ''
+GLOVE_DIR = os.path.join(BASE_DIR, 'glove.6B')
+# TEXT_DATA_DIR = os.path.join(BASE_DIR, '20_newsgroup')
+MAX_SEQUENCE_LENGTH = 30
+MAX_NUM_WORDS = 20000
+EMBEDDING_DIM = 100
+VALIDATION_SPLIT = 0.2
+
+
+def embedding_layer(word_index):
+    print('Indexing word vectors.')
+    embeddings_index = {}
+    with open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt')) as f:
+        for line in f:
+            word, coefs = line.split(maxsplit=1)
+            coefs = np.fromstring(coefs, 'f', sep=' ')
+            embeddings_index[word] = coefs
+
+    print('Preparing embedding matrix.')
+
+    # prepare embedding matrix
+    num_words = min(MAX_NUM_WORDS, len(word_index) + 1)
+    embedding_matrix = np.zeros((num_words, EMBEDDING_DIM))
+    for word, i in word_index.items():
+        if i >= MAX_NUM_WORDS:
+            continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+
+    return Embedding(num_words,
+                                EMBEDDING_DIM,
+                                embeddings_initializer=Constant(embedding_matrix),
+                                input_length=MAX_SEQUENCE_LENGTH,
+                                trainable=False)
